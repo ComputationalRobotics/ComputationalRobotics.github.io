c91006fe2d15c5b508f6aca6a3c2b3f349fa07b8
yifantong1010 <yifantong1010@gmail.com>
2024-02-24 11:35:38 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react'; // Importing the React library
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importing BrowserRouter for SPA routing, Routes for route definitions, Route for a single route
-import Header from './components/Header'; // Importing Header component to be placed at the top of every page
-import HomePage from './components/HomePage'; // Importing HomePage component for the root path
-import PeoplePage from './components/PeoplePage'; // Importing PeoplePage component for '/people' path
-import ResearchPage from './components/ResearchPage'; // Importing ResearchPage component for '/research' path
-import JoiningPage from './components/JoiningPage'; // Importing JoiningPage component for '/contact' path
-import PublicationsPage from './components/PublicationsPage'; // Importing PublicationsPage component for '/publications' path
-import Footer from './components/Footer'; // Importing Footer component to be placed at the bottom of every page
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importing necessary components from react-router-dom
+import Header from './components/Header'; // Importing Header component
+import HomePage from './components/HomePage'; // Importing HomePage component
+import PeoplePage from './components/PeoplePage'; // Importing PeoplePage component
+import ResearchPage from './components/ResearchPage'; // Importing ResearchPage component
+import JoiningPage from './components/JoiningPage'; // Importing JoiningPage component
+import PublicationsPage from './components/PublicationsPage'; // Importing PublicationsPage component
+import Footer from './components/Footer'; // Importing Footer component
+import './App.css'; // Importing the main stylesheet
 
-// Importing the main stylesheet for styling the app
-import './App.css';
-
-// Defining the main App component
 function App() {
   return (
     <Router>
-      <Header /> {/* Placing the Header component that will appear across all routes*/}
-      <main> {/* This main tag will wrap the Routes and allow for the sticky footer */}
+      <Header />
+      <main>
         <Routes>
-          <Route path={process.env.PUBLIC_URL + "/people"} element={<PeoplePage />} />
-          {/* <Route path={process.env.PUBLIC_URL + "/research"} element={<ResearchPage />} /> */}
-          <Route path={process.env.PUBLIC_URL + "/publications"} element={<PublicationsPage />} />
-          <Route path={process.env.PUBLIC_URL + "/joining"} element={<JoiningPage />} />
-          <Route path={process.env.PUBLIC_URL + "/"} element={<HomePage />} />
+          <Route path={`${process.env.PUBLIC_URL}/people`} element={<PeoplePage />} />
+          {/* <Route path={`${process.env.PUBLIC_URL}/research`} element={<ResearchPage />} /> */}
+          <Route path={`${process.env.PUBLIC_URL}/publications`} element={<PublicationsPage />} />
+          <Route path={`${process.env.PUBLIC_URL}/joining`} element={<JoiningPage />} />
+          <Route path={`${process.env.PUBLIC_URL}/`} element={<HomePage />} />
         </Routes>
       </main>
       <Footer />
