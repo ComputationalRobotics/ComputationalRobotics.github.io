415ac6eee3eeac24f78b07bdc9490298fd648547
Heng Yang <hankyang94@gmail.com>
2024-02-24 09:54:21 -0500

    check

---
+++ build/precache-manifest.0b1bc0eb126d6202d791f67fb9e5e6ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bae19954e45dbd85ef0f4b67cfc248",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbac693470cd78a1d67",
+    "url": "/static/css/main.27eaa2c8.chunk.css"
+  },
+  {
+    "revision": "b4107b35021f170e784d",
+    "url": "/static/js/2.5d7d95c3.chunk.js"
+  },
+  {
+    "revision": "33c9af1c367bdbf7cda8de087498f4d5",
+    "url": "/static/js/2.5d7d95c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03bfb0d93108a4441ee",
+    "url": "/static/js/3.f28a6d04.chunk.js"
+  },
+  {
+    "revision": "cdbac693470cd78a1d67",
+    "url": "/static/js/main.fc93e33b.chunk.js"
+  },
+  {
+    "revision": "187b91e1e4b317cf8b9b",
+    "url": "/static/js/runtime-main.58491982.js"
+  }
+]);
