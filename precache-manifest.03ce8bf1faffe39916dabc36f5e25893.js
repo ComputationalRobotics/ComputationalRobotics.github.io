9bddd434b7566ac1de3e0afc0cabc12cfa365a30
Heng Yang <hankyang94@gmail.com>
2024-04-13 10:09:43 -0400

    Updates

---
+++ precache-manifest.03ce8bf1faffe39916dabc36f5e25893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0247d7b1ffae243bb7f63c78e39daea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60d633f1fb42b7c89ff2",
+    "url": "/static/css/main.c7e50295.chunk.css"
+  },
+  {
+    "revision": "b4107b35021f170e784d",
+    "url": "/static/js/2.5d7d95c3.chunk.js"
+  },
+  {
+    "revision": "33c9af1c367bdbf7cda8de087498f4d5",
+    "url": "/static/js/2.5d7d95c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03bfb0d93108a4441ee",
+    "url": "/static/js/3.f28a6d04.chunk.js"
+  },
+  {
+    "revision": "60d633f1fb42b7c89ff2",
+    "url": "/static/js/main.f70e66cd.chunk.js"
+  },
+  {
+    "revision": "187b91e1e4b317cf8b9b",
+    "url": "/static/js/runtime-main.58491982.js"
+  }
+]);
